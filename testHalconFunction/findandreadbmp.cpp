b531b4d0a35812e77d9f0f06607cb01d73c3e758
LawYukwang <lyh516148@163.com>
2016-03-24 08:07:08 +0800

    添加文件名保存操作

--- testHalconFunction/findandreadbmp.cpp
+++ testHalconFunction/findandreadbmp.cpp
@@ -7,22 +7,24 @@
 using namespace Halcon;
 
 std::list<Hobject> Images;
+std::list<char *> imageNames;
 
 int main()
 {
-	const char *strNonTempletImgDir = "E:\\Action\\testdataset\\´¿É«\\";
+	const char *strNonTempletImgDir = "E:\\Images\\ÏàÆ¬\\";
 	long file;
     struct _finddata_t find;
 	char path[MAX_PATH];
 	strcpy_s(path, MAX_PATH, strNonTempletImgDir);
 	int len = strnlen_s(path, MAX_PATH);
-	strcat_s(path, len + 6, "*.bmp");
+	strcat_s(path, len + 6, "*.jpg");
 	if((file=_findfirst(path, &find))!=-1L)
 	{
 		char bmpFile[MAX_PATH];
 		strcpy_s(bmpFile, MAX_PATH, strNonTempletImgDir);
 		len = strnlen_s(bmpFile, MAX_PATH);
 		strcat_s(bmpFile, len + strnlen_s(find.name, MAX_PATH) + 1, find.name);
+		imageNames.push_back(find.name);
 		Hobject image;
 		read_image(&image,&bmpFile[0]);
 		Images.push_back(image);
@@ -32,6 +34,7 @@ int main()
 			strcpy_s(bmpFile, MAX_PATH, strNonTempletImgDir);
 			len = strnlen_s(bmpFile, MAX_PATH);
 			strcat_s(bmpFile, len + strnlen_s(find.name, MAX_PATH) + 1, find.name);
+			imageNames.push_back(find.name);
 			Hobject image;
 			read_image(&image,&bmpFile[0]);
 			Images.push_back(image);
